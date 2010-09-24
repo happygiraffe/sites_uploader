bf24be364a86b3d48eb651d54d49c308c67b28dd
Dominic Mitchell <dom@happygiraffe.net>
2010-09-24 17:34:56 +0100

    Document; refactor slightly.

--- socket_test.py
+++ socket_test.py
@@ -6,32 +6,50 @@ import cgi
 import socket
 import BaseHTTPServer
 
-class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-  def log_message(self, format, *args):
-    """Don't log anything."""
-    pass
 
-  def do_GET(self):
-    params = cgi.parse_qs(self.path.split('?')[1])
-    self.server.result = params
-    msg = '<h1>All Done</h1>\n<p>Thanks!  You may close this window now.</p>\n'
-    self.send_response(200)
-    self.send_header('Content-Length', str(len(msg)))
-    self.end_headers()
-    print >>self.wfile, msg
+class ParamsReceiverServer(BaseHTTPServer.HTTPServer):
+  """A web server that spins up just to act as a callback URL.
 
-class MyServer(BaseHTTPServer.HTTPServer):
-  def __init__(self, handler_class=MyHandler):
+  When it receives a response, it exits, returning back to the previous
+  flow of control.  This is very useful for OAuth callbacks.
+
+  Methods:
+    my_url: The URL to use as a callback.
+    serve_until_result: The main web serving loop.
+
+  Extra properties:
+    result: The query parameters from the callback URL.
+  """
+
+  class ParamsReceiverHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def log_message(self, format, *args):
+      """Don't log anything."""
+      pass
+
+    def do_GET(self):
+      """Capture the query string and return."""
+      if '?' in self.path:
+        params = cgi.parse_qs(self.path.split('?')[1])
+        if params:
+          self.server.result = params
+      msg = '<h1>All Done</h1>\n<p>Thanks!  You may close this window now.</p>\n'
+      self.send_response(200)
+      self.send_header('Content-Length', str(len(msg)))
+      self.end_headers()
+      print >>self.wfile, msg
+
+  def __init__(self, handler_class=ParamsReceiverHandler):
+    self.result = None
     self.port = self.get_random_port()
     server_address = ('', self.port)
     # Darned old-style classes.
     BaseHTTPServer.HTTPServer.__init__(self, server_address, handler_class)
-    # For returning a result.
-    self.result = None
 
   def get_random_port(self):
-    # Make an anonymous socket on a random port.  Once closed, that port is
-    # free for us to use for a while.
+    """Get a random port number to listen on."""
+    # Calling bind to port zero asks the kernel to allocate a port number
+    # for us.  Once it's done so, that's ours until the kernel loops around
+    # port numbers again.  So long as we've called listen by then, it's fine.
     sock = socket.socket()
     sock.bind(('', 0))
     port = sock.getsockname()[1]
@@ -39,19 +57,21 @@ class MyServer(BaseHTTPServer.HTTPServer):
     return port
 
   def my_url(self):
+    """What URL is this server listening on?"""
     return 'http://%s:%d' % (socket.getfqdn(), self.port)
 
   def serve_until_result(self):
+    """Keep serving until a query string has been captured."""
     while not self.result:
       self.handle_request()
 
 
 def main():
-  httpd = MyServer()
+  httpd = ParamsReceiverServer()
   print httpd.my_url()
   httpd.serve_until_result()
-  
-  print 'result: [%s]' % str(httpd.result)
+
+  print 'result: %s' % str(httpd.result)
 
 if __name__ == '__main__':
   main()
